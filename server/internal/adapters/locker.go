6a72aec928031a54b262450c83f7cc1ca5a07413
Evans Owamoyo <lordvidex.prime@gmail.com>
2022-12-07 09:47:25 +0300

    profiling the server: cleanups

--- server/internal/adapters/locker.go
+++ server/internal/adapters/locker.go
@@ -14,43 +14,42 @@ func NewLocker(c context.Context, cleanupFrequency time.Duration) *Locker {
 	l := &Locker{}
 
 	// regular cleanups
-	go func() {
-		tick := time.NewTicker(cleanupFrequency)
-		for {
-			select {
-			case <-tick.C:
-				l.mx.Range(func(key, value interface{}) bool {
-					m, ok := value.(*sync.Mutex)
-					if !ok {
-						return true // continue
-					}
-					if m.TryLock() {
-						m.Unlock()
-						l.mx.Delete(key)
-					}
-					return true
-				})
-			case <-c.Done():
-				return
-			}
-		}
-	}()
+	go l.cleanup(c, cleanupFrequency)
 	return l
+}
+
+func (l *Locker) cleanup(c context.Context, frequency time.Duration) {
+	tick := time.NewTicker(frequency)
+	for {
+		select {
+		case <-tick.C:
+			l.mx.Range(func(key, value interface{}) bool {
+				m, ok := value.(*sync.Mutex)
+				if !ok {
+					return true // continue
+				}
+				if m.TryLock() {
+					m.Unlock()
+					l.mx.Delete(key)
+				}
+				return true
+			})
+		case <-c.Done():
+			return
+		}
+	}
 }
 
 func (l *Locker) Lock(x any, y ...any) func() {
 	locks := make([]*sync.Mutex, len(y)+1)
-	lock, _ := l.mx.LoadOrStore(x, &sync.Mutex{})
-	lock.(*sync.Mutex).Lock()
-	locks[0] = lock.(*sync.Mutex)
-	for i, key := range y {
-		lock, _ = l.mx.LoadOrStore(key, &sync.Mutex{})
+	for i, key := range append([]any{x}, y...) {
+		lock, _ := l.mx.LoadOrStore(key, &sync.Mutex{})
 		lock.(*sync.Mutex).Lock()
-		locks[i+1] = lock.(*sync.Mutex)
+		locks[i] = lock.(*sync.Mutex)
 	}
 	return func() {
-		for _, lock = range locks {
-			lock.(*sync.Mutex).Unlock()
+		for _, lock := range locks {
+			lock.Unlock()
 		}
 	}
 }
