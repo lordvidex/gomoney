79778cd67e246184ecdf5b90b232d5bac40b51a2
Evans Owamoyo <lordvidex.prime@gmail.com>
2023-05-14 13:30:47 +0300

    testing with lockgroups

--- server/internal/adapters/locker.go
+++ server/internal/adapters/locker.go
@@ -7,12 +7,15 @@ import (
 )
 
 type Locker struct {
+	// mx is used for locking mutexes for a lockgroup
 	mx sync.Map
+
+	// groups is for storing assigning a single item to a lockgroup
+	groups sync.Map
 }
 
 func NewLocker(c context.Context, cleanupFrequency time.Duration) *Locker {
 	l := &Locker{}
-
 	// regular cleanups
 	go l.cleanup(c, cleanupFrequency)
 	return l
@@ -41,15 +44,34 @@ func (l *Locker) cleanup(c context.Context, frequency time.Duration) {
 }
 
 func (l *Locker) Lock(x any, y ...any) func() {
-	locks := make([]*sync.Mutex, len(y)+1)
-	for i, key := range append([]any{x}, y...) {
-		lock, _ := l.mx.LoadOrStore(key, &sync.Mutex{})
-		lock.(*sync.Mutex).Lock()
-		locks[i] = lock.(*sync.Mutex)
-	}
-	return func() {
-		for _, lock := range locks {
-			lock.Unlock()
+	items := append([]any{x}, y...)
+
+	for _, item := range items {
+		grp, ok := l.groups.Load(item)
+		if !ok {
+			continue
 		}
+		if grp == nil {
+			l.groups.Delete(item)
+			continue
+		}
+		cast, ok := grp.(*sync.Mutex)
+		if !ok {
+			continue
+		}
+		cast.Lock()   // waits for the group to be unlocked
+		cast.Unlock() // unlocks the group
+		continue
 	}
+	// locks := make([]*sync.Mutex, len(y)+1)
+	// for i, key := range append([]any{x}, y...) {
+	// 	lock, _ := l.mx.LoadOrStore(key, &sync.Mutex{})
+	// 	lock.(*sync.Mutex).Lock()
+	// 	locks[i] = lock.(*sync.Mutex)
+	// }
+	// return func() {
+	// 	for _, lock := range locks {
+	// 		lock.Unlock()
+	// 	}
+	// }
 }
